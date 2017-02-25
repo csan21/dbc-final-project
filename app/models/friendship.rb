99843bdf9975319a38c2205496c662e34a4f7068
Chris Santos & Nick Devlin <pair+csan21.nickdevlin@devbootcamp.com>
2017-02-25 12:52:32 -0600

    Create associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,4 @@
 class Friendship < ApplicationRecord
+  belongs_to :adder, class_name: :User
+  belongs_to :accepter, class_name: :User
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,4 @@
 class Poll < ApplicationRecord
+  has_many :answers
+  belongs_to :final_choice, class_name: :Answer
 end
