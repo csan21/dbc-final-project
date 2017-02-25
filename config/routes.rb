f3926442b2c9bc185d7bc8fcef8279a57c0fa745
Pavan Sarguru <pavan.sarguru@gmail.com>
2017-02-25 10:30:45 -0600

    initial travis integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "home#index"
+  # Don't know why this is here?
+  # root to: "home#index"
 
   resource :messages do
     collection do
