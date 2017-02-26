bbdc3a11e7168f40ef89fe45ef81da4585785b6c
Pavan Sarguru <pavansarguru@gmail.com>
2017-02-26 15:12:14 -0600

    edit user migration and add token methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :reset_token
+  before_save   :downcase_email
+  before_create :generate_access_token
+
   has_many :created_polls, class_name: :Poll, foreign_key: :creator_id
   has_many :votes
   has_many :chosen_answers, through: :votes, source: :answer
@@ -32,4 +36,42 @@ class User < ApplicationRecord
   def taken_polls
     self.chosen_answers.map { |answer| answer.poll }
   end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def generate_access_token
+      begin
+        self.access_token = User.new_token
+      end while self.class.exists?(access_token: access_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,12 @@ ActiveRecord::Schema.define(version: 20170224235222) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "invite_code"
+    t.string   "remember_digest"
+    t.string   "access_token"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.string   "phone_number"
+    t.index ["access_token"], name: "index_users_on_access_token", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
