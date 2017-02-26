6073d4ddfc5caf957d7b5832804c94e587eaccf5
Chris Santos & Nick Devlin <pair+csan21.nickdevlin@devbootcamp.com>
2017-02-25 22:28:24 -0600

    create new poll functionality

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,5 +1,24 @@
 class PollsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  #skip_before_filter :authenticate_user!, :only => "reply"
+
   def show
     @poll = Poll.find(params[:id])
+  end
+
+  def new
+    @poll = Poll.new
+  end
+
+  def create
+    @poll = Poll.create(
+      question: params["question"],
+      expiration: Time.now + params["expiration"].to_i,
+      creator_id: 1
+    )
+    Answer.create(text: params["answer1"], poll_id: @poll.id)
+    Answer.create(text: params["answer2"], poll_id: @poll.id)
+
+    redirect_to "/users/1"
   end
 end
