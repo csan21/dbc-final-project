000bd8d831ea61d1c0a1568635f4077736908ee6
Pavan Sarguru <pavansarguru@gmail.com>
2017-02-26 10:59:51 -0600

    create api routes and api controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
 
   resources :users, :polls, :answers
   resources :votes, only: [:create]
+
+  namespace :api do
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+    get    'verify'  => 'sessions#verify_access_token'
+    resources :users, param: :access_token
+    resources :password_resets, only: [:new, :create, :edit, :update]
+  end
 end
