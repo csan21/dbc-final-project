c0985d7b1eaeadbada4aea2fc4818f258ca5424f
Cody Fuller <mail@codyfuller.com>
2017-02-28 10:20:11 -0600

    Add image uploads to s3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224231229) do
+ActiveRecord::Schema.define(version: 20170228014909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,17 @@ ActiveRecord::Schema.define(version: 20170224231229) do
   end
 
   create_table "polls", force: :cascade do |t|
-    t.string   "question",                  null: false
-    t.datetime "expiration",                null: false
+    t.string   "question",                          null: false
+    t.datetime "expiration",                        null: false
     t.string   "comment"
-    t.integer  "creator_id",                null: false
-    t.boolean  "active?",    default: true
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.integer  "creator_id",                        null: false
+    t.boolean  "active?",            default: true
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
